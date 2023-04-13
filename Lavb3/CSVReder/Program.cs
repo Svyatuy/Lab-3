f3df8b82f2010bef5bd1d2d131405d392cd7c083
Svyatuy <112246649+Svyatuy@users.noreply.github.com>
2023-04-13 22:32:39 +0300

    Lab-3
    Lab-3

---
+++ Lavb3/CSVReder/Program.cs
@@ -0,0 +1,15 @@
+﻿using CSVReader;
+
+namespace Lab9
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Denis\Desktop\Study\2k_2h\Lab_3_h2\Lab_3_h2\CSVReder\test.csv";
+            CSVReadering cSVReadering = new CSVReadering();
+            cSVReadering.Reader(path);
+        }
+
+    }
+}
